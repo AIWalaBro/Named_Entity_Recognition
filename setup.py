3e696c41d038e4452f509815a20222cf5a34ebbf
AIWalaBro <aiwalabro@gmail.com>
2024-04-02 18:04:02 +0530

    setup file updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ner-using-bert",
+    version="0.0.1",
+    author="Bharatbhushan",
+    author_email="bkbhushan111@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
