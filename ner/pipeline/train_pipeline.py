1922023920a0f3310afe7c75717e80e07d793936
AIWalaBro <aiwalabro@gmail.com>
2024-04-09 16:35:08 +0530

    training pipeline intiated

---
+++ ner/pipeline/train_pipeline.py
@@ -0,0 +1,45 @@
+import sys
+from ner.components.data_ingestion import DataIngestion
+from ner.configuration.gcloud import GCloud
+from ner.constants import *
+from ner.entity.artifact_entity import (DataIngestionArtifacts)
+from ner.entity.config_entity import DataIngestionConfig
+from ner.exception import NerException
+from ner.logger import logging
+
+class TrainPipeline:
+    def __init__(self):
+        self.data_ingestion_config = DataIngestionConfig()
+        self.gcloud = GCloud()
+        
+    # starting second method data ingestion
+    
+    def start_data_ingestion(self) -> DataIngestionArtifacts:
+        logging.info("Entered the start_data_ingestion method of train pipeline class")
+        try:
+            logging.info("getting the data from google cloud storage")
+            
+            data_ingestion = DataIngestion(
+                data_ingestion_config=self.data_ingestion_config, gcloud=self.gcloud
+            )
+            
+            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+            logging.info(f"Got the file from Google cloud storage. File name - {os.path.basename(self.data_ingestion_config.gcp_data_file_path)}")
+            logging.info(
+                'Exited the start_data_ingestion method of TrainPipeline class'
+            )
+            return data_ingestion_artifact
+        except Exception as e:
+            raise NerException(e, sys) from e
+        
+        
+    # this method is use to start the training pipeline
+    
+    def run_pipeline(self) -> None:
+        try:
+            logging.info('started model training >>>>>>>>>>>>>>>>>>')
+            data_ingestion_artifact = self.start_data_ingestion()
+            
+        except Exception as e:
+            raise NerException(e, sys) from e
+    
