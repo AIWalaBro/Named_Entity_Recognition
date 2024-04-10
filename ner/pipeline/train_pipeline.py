1fc53f7cae324119eb63b8f9760cacaa76f39aa9
AIWalaBro <aiwalabro@gmail.com>
2024-04-10 14:45:13 +0530

    updated training pipeline

--- ner/pipeline/train_pipeline.py
+++ ner/pipeline/train_pipeline.py
@@ -2,14 +2,17 @@ import sys
 from ner.components.data_ingestion import DataIngestion
 from ner.configuration.gcloud import GCloud
 from ner.constants import *
-from ner.entity.artifact_entity import (DataIngestionArtifacts)
-from ner.entity.config_entity import DataIngestionConfig
+from ner.entity.artifact_entity import DataIngestionArtifacts, DataTransformationArtifacts
+from ner.entity.config_entity import DataIngestionConfig, DataTransformationConfig
 from ner.exception import NerException
 from ner.logger import logging
+
+from ner.components.data_transforamation import DataTransformation
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_transformation_config = DataTransformationConfig()
         self.gcloud = GCloud()
         
     # starting second method data ingestion
@@ -33,12 +36,44 @@ class TrainPipeline:
             raise NerException(e, sys) from e
         
         
+    
+     # This method is used to start the data validation
+    def start_data_transformation(
+        self, data_ingestion_artifact: DataIngestionArtifacts
+    ) -> DataTransformationArtifacts:
+        logging.info(
+            "Entered the start_data_transformation method of TrainPipeline class"
+        )
+        try:
+            data_transformation = DataTransformation(
+                data_transformation_config=self.data_transformation_config,
+                data_ingestion_artifacts=data_ingestion_artifact,
+            )
+
+            data_transformation_artifact = (
+                data_transformation.initiate_data_transformation()
+            )
+
+            logging.info("Performed the data validation operation")
+            logging.info(
+                "Exited the start_data_transformation method of TrainPipeline class"
+            )
+            return data_transformation_artifact
+
+        except Exception as e:
+            raise NerException(e, sys) from e
+        
+        
+        
     # this method is use to start the training pipeline
     
     def run_pipeline(self) -> None:
         try:
             logging.info('started model training >>>>>>>>>>>>>>>>>>')
             data_ingestion_artifact = self.start_data_ingestion()
+            data_transformation_artifact = self.start_data_transformation(
+                data_ingestion_artifact = data_ingestion_artifact
+            )
             
         except Exception as e:
             raise NerException(e, sys) from e
