12774b8e753ec5db408ef74836a8d2589b5fac53
AIWalaBro <aiwalabro@gmail.com>
2024-04-15 15:25:57 +0530

    artifact entity upadted for model trainer

--- ner/entity/artifact_entity.py
+++ ner/entity/artifact_entity.py
@@ -17,3 +17,34 @@ class DataTransformationArtifacts:
     df_val_path: str
     df_test_path: str
     unique_labels_path: str
+    
+    
+# it will return model path and tokenizer path
+from dataclasses import dataclass
+
+
+# Data Ingestion Artifacts
+@dataclass
+class DataIngestionArtifacts:
+    zip_data_file_path: str
+    csv_data_file_path: str
+
+
+
+# Data Transformation Artifacts
+@dataclass
+class DataTransformationArtifacts:
+    labels_to_ids_path: str
+    ids_to_labels_path: str
+    df_train_path: str
+    df_val_path: str
+    df_test_path: str
+    unique_labels_path: str
+
+
+
+# Model Training Artifacts
+@dataclass
+class ModelTrainingArtifacts:
+    bert_model_path: str
+    tokenizer_file_path: str
